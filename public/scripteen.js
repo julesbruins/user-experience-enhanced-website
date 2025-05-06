2d3dc22d137edf69170b4c4cac571c8306ef39c9
julesbruins <jf-bruins@hotmail.com>
2025-05-06 17:26:07 +0200

    loading state try outs

--- public/scripteen.js
+++ public/scripteen.js
@@ -1,26 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const successMessage = document.querySelector(".succes-message");
- 
-//     if (successMessage) {
-//         setTimeout(() => {
-//             successMessage.classList.add('show');
-//         }, 4);
- 
-//         setTimeout(() => {
-//             successMessage.classList.add('fade-out');
-//         }, 2400);
-//     }
-
-//     const openButton = document.querySelector('.open-button')
-//     console.log("openButton",openButton);
-//     const container = document.querySelector('.nav-container')
-
-//     openButton.addEventListener('click', () => {
-//     container.classList.toggle('active')
-// })
-// });
-
-
 // POST progressive enhanced
 document.documentElement.classList.add("js");
 
@@ -65,7 +42,6 @@ button.addEventListener("click", async function(event) {
    event.preventDefault()
 
    let formData = new FormData(postForm)
-   console.log("formData", formData)
 
     const response = await fetch(postForm.action, {
         method: postForm.method,
@@ -73,16 +49,12 @@ button.addEventListener("click", async function(event) {
       })
     
     const responseText = await response.text()
-    console.log("responseText", responseText)
-
     const parser = new DOMParser()
     const responseDOM = parser.parseFromString(responseText, 'text/html')
-    console.log("responseDOM", responseDOM)
 
     const newMessage = responseDOM.querySelector("main article:last-of-type")
-    console.log("new message", newMessage)
     const lastMessage = document.querySelector("main article:last-of-type")
-    console.log("lastmessage", lastMessage)
+
     lastMessage.after(newMessage)
     form.classList.toggle("is-visible");
 
@@ -91,6 +63,10 @@ button.addEventListener("click", async function(event) {
 
     formNameField.value = ""
     formMessageField.value = ""
+
+    const loadingState = button.querySelector(".loading-state")
+    loadingState.classList.add("show")
+    loadingState.classList.remove("hide")
 
 })
 
